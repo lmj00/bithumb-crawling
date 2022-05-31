a083a6466e63d0e218a15541aa70e4d203effffa
이민진 <e.minjin00@gmail.com>
2022-05-31 19:03:08 +0900

    Feat: HomeController 추가

---
+++ src/main/java/crawling/coin/controller/HomeController.java
@@ -0,0 +1,15 @@
+package crawling.coin.controller;
+
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
