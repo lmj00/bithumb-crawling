ba873c99f8ee41c75b2e6c68dc3fce272957a774
이민진 <e.minjin00@gmail.com>
2022-06-03 15:44:36 +0900

    Feat: smallName entity 추가

--- src/main/java/crawling/coin/domain/Coin.java
+++ src/main/java/crawling/coin/domain/Coin.java
@@ -20,6 +20,7 @@ public class Coin {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String smallName;
     private String name;
     @Column(name = "real_krw")
     private BigDecimal realkrw;
