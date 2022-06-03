40629b3875da21d9b2031d052193297771d2b473
이민진 <e.minjin00@gmail.com>
2022-06-03 15:49:39 +0900

    Feat: coinSmall_KRW save, builder 추가

--- src/main/java/crawling/coin/service/CoinSaveService.java
+++ src/main/java/crawling/coin/service/CoinSaveService.java
@@ -15,15 +15,19 @@ public class CoinSaveService {
 
     private final CoinRepository coinRepository;
 
-    public void save(List<String> coinList_KRW,
-                     List<BigDecimal> real_KRW,
-                     List<Float> assetRealPrice_KRW,
-                     List<Float> assetRealRate_KRW,
-                     List<Long> tradePrice_KRW,
-                     List<Long> assetTotalPrice_KRW) {
+    public void save(
+            List<String> coinSmall_KRW,
+            List<String> coinList_KRW,
+            List<BigDecimal> real_KRW,
+            List<Float> assetRealPrice_KRW,
+            List<Float> assetRealRate_KRW,
+            List<Long> tradePrice_KRW,
+            List<Long> assetTotalPrice_KRW
+    ) {
 
         for (int j = 0; j < coinList_KRW.size(); j++) {
             Coin coin = Coin.builder()
+                    .smallName(coinSmall_KRW.get(j))
                     .name(coinList_KRW.get(j))
                     .realkrw(real_KRW.get(j))
                     .realPrice(assetRealPrice_KRW.get(j))
