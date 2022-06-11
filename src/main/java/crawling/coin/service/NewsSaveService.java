a7a4ba2ead90753101ea29b01deb7b02e81df347
이민진 <e.minjin00@gmail.com>
2022-06-12 05:11:23 +0900

    Feat: NewsSaveService 추가

---
+++ src/main/java/crawling/coin/service/NewsSaveService.java
@@ -0,0 +1,30 @@
+package crawling.coin.service;
+
+import crawling.coin.domain.News;
+import crawling.coin.repository.NewsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class NewsSaveService {
+    private final NewsRepository newsRepository;
+
+    public void save(
+            String title,
+            String editor,
+            String date,
+            String link
+    ) {
+            News news = News.builder()
+                    .title(title)
+                    .editor(editor)
+                    .date(date)
+                    .link(link)
+                    .build();
+
+            newsRepository.save(news);
+    }
+}
+
