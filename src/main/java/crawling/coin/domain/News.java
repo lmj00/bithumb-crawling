e019df09d0ce6445a1c45c67fcbef905f4802512
이민진 <e.minjin00@gmail.com>
2022-06-12 04:59:20 +0900

    Feat: news entity 어노테이션 추가

--- src/main/java/crawling/coin/domain/News.java
+++ src/main/java/crawling/coin/domain/News.java
@@ -1,13 +1,25 @@
 package crawling.coin.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
+import javax.persistence.*;
+
 @Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class News {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String title;
+    private String editor;
     private String date;
     private String link;
 
